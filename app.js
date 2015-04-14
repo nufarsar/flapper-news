81fd69595627a01fe4c685354bad3d2db3cd123f
Allan Reyes <allanbreyes@gmail.com>
2015-04-14 06:59:18 -0500

    add comments form and upvoting

--- app.js
+++ app.js
@@ -68,5 +68,22 @@ app.controller('PostsController', [
 
   function($scope, $stateParams, posts) {
     $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function() {
+      if ($scope.body === '') {
+        return;
+      } else {
+        $scope.post.comments.push({
+          body: $scope.body,
+          author: 'user',
+          upvotes: 0
+        });
+        $scope.body = '';
+      }
+    };
+
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
   }
 ]);

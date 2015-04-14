d363bd2aa12b932fb9ee9839e35bf08c9aacbbd2
Allan Reyes <allanbreyes@gmail.com>
2015-04-14 05:45:58 -0500

    add post and link submission

--- app.js
+++ app.js
@@ -10,5 +10,23 @@ app.controller('MainController', [
       { title: 'post 4', upvotes: 9},
       { title: 'post 5', upvotes: 4}
     ];
+
+    $scope.addPost = function() {
+      if (!$scope.title || $scope.title === '') {
+        return;
+      } else {
+        $scope.posts.push({
+          title: $scope.title,
+          link: $scope.link,
+          upvotes: 0
+        });
+        $scope.title = '';
+        $scope.link = '';
+      }
+    };
+
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
   }
 ]);

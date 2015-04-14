943bbcd88d38c023fe8ae6ed9f50bdef39235c14
Allan Reyes <allanbreyes@gmail.com>
2015-04-14 12:26:40 -0500

    add post upvoting

--- app/assets/javascripts/ngApp.js
+++ app/assets/javascripts/ngApp.js
@@ -32,18 +32,28 @@ app.factory('posts', [
     var o = {
       posts: []
     };
+
     o.getAll = function() {
       return $http.get('/posts.json')
         .success(function(data) {
           angular.copy(data, o.posts);
         });
     };
+
     o.create = function(post) {
       return $http.post('/posts.json', post)
         .success(function(data){
           o.posts.push(data);
         });
     };
+
+    o.upvote = function(post) {
+      return $http.put('/posts/' + post.id + '/upvote.json')
+        .success(function(data) {
+          post.upvotes += 1;
+        });
+    };
+
     return o;
   }
 ]);
@@ -70,7 +80,7 @@ app.controller('MainController', [
     };
 
     $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
+      posts.upvote(post);
     };
   }
 ]);
